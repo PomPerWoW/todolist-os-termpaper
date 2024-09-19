b31c7f31cfb1ccf84c1717dafffa3c47860cd9f7
Suriya Chaubey <64011643@kmitl.ac.th>
2024-09-19 17:28:58 +0700

    fix cors error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -30,6 +30,7 @@ def create_app():
                     "http://127.0.0.1:8080",
                     "http://127.0.0.1:5500",
                     "http://localhost:5500",
+                    "http://localhost:8080" 
                 ]
             }
         },
